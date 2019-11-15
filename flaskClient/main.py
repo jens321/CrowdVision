6bfef8410fef2c48c44f91bb1709f68890a2afc2
Jens Tuyls <you@example.com>
2019-11-15 11:55:54 -0800

    add a lot of stuff

---
+++ flaskClient/main.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import cv2
+
+def send_img(filename):
+    addr = 'http://127.0.0.1:5000'
+    test_url = addr + '/api/test'
+
+    content_type = 'image/jpeg'
+    headers = {'content-type': content_type}
+
+    img = cv2.imread(filename)
+
+    _, img_encoded = cv2.imencode('.jpg', img)
+
+    response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+
+    print(json.loads(response.text))
+
+if __name__ == "__main__":
+    send_img('microsoft.jpeg')
